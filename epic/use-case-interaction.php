82b8d91c217d004ba86ca3e7ec1fc46b863de51b
Jacob Lott <jlott3@cnm.edu>
2020-01-14 19:19:54 -0700

    Added precondition and post condition. Also added the website interaction flow.

--- epic/use-case-interaction.php
+++ epic/use-case-interaction.php
@@ -6,16 +6,24 @@
 </head>
 <body>
 <h1>Use Case/Interaction Flow</h1>
+<p>precondition: Desiree is on the home page of eBay.</p>
+<p>post condition: Desiree has a submitted order of Red Checkered Slip On Vans Shoes!</p>
 <p>Desiree goes on to the home page of eBay.</p>
 <p>She clicks Browse by Category, and under Fashion, she clicks on shoes.</p>
+<p>Website loads new page with images of shoes.</p>
 <p>She clicks on Casual Shoes, and in the category Top Shoe Brands, she clicks Vans.</p>
+<p>Website loads new page with images of Vans shoes.</p>
 <p>She clicks on Vans Slip On, and in the Price settings on the left, she edits the price range from $30-$50.</p>
+<p>Website loads new page with images of Vans Slip On Shoes ranging from $30-$50.</p>
 <p>She scrolls through the first page, and doesn't see the shoes. She then continues to Page Two.</p>
+<p>Website loads new page with different images of Vans Slip On Shoes ranging from $30-$50.</p>
 <p>She scrolls down, and sees the shoes she is looking for, for $30! They are used however, so she clicks on it to
     check it out.</p>
+<p>Website loads new page showing the Red Checkered Slip On Vans Shoe that is $30.</p>
 <p>She looks at the pictures of the shoes, and they look almost new! The description says that they are barely
     used, and were only worn once, and the owner is selling them because he didn't like the shoes.</p>
 <p>She saw that the seller has a 99% Positive Rating. Desiree now trusts the seller, and knows that the shoes will
 be in great condition for the price she was asking! She continued to buy the shoes.</p>
+<p>Website loads the checkout page to buy the shoes.</p>
 </body>
 </html>
