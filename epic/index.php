bce0b4eaa92d9e953364e18fa06e44eb153b23df
Jacob Lott <jlott3@cnm.edu>
2020-01-16 13:03:35 -0700

    Added (foreign key) elements.

--- epic/index.php
+++ epic/index.php
@@ -9,5 +9,6 @@
 <p><a href="persona.php">Persona</a></p>
 <p><a href="use-case-interaction.php">Use Case/Interaction Flow</a></p>
 <p><a href="user-story.php">User Story</a></p>
+<p><a href="conceptualmodel.html">Conceptual Model</a></p>
 </body>
 </html>
